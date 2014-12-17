e38414eca734831042056562884b624271394f96
jmiedel <jmiedel@andrew.cmu.edu>
2014-12-17 15:14:38 -0500

    added motion

--- static/js/rockstorm.js
+++ static/js/rockstorm.js
@@ -7,8 +7,30 @@ function preload() {
 }
 
 function create() {
-  game.add.sprite(0, 0, 'smiley');
+  player = game.add.sprite(0, 0, 'smiley');
+  //  We're going to be using physics, so enable the Arcade Physics system
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.enable(player);
 }
 
 function update() {
+  cursors = game.input.keyboard.createCursorKeys();
+  if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+ 
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+ 
+    }
+  
+    else 
+    {
+        player.body.velocity.x = 0;
+ 
+    }
 }
