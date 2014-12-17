33b4bece2dc97e5a633926a62aa0419a52ce90c7
Alvin Wang <alvin7@gmail.com>
2014-12-17 14:48:40 -0500

    rockstorm created

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.use('/', express.static(__dirname + '/static/'));
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/static/rockstorm.html');
+  res.sendFile(__dirname + '/static/rockstorm.html');
 })
 
 var server = app.listen(3000, function () {
