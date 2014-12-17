b9aa298a8fb398f190b9acef3d10783478a309b9
Alvin Wang <alvin7@gmail.com>
2014-12-17 14:36:40 -0500

    set up basic file serving

--- server.js
+++ server.js
@@ -5,9 +5,7 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use('/', express.static(__dirname + '/static/'));
 
 var server = app.listen(3000, function () {
 
