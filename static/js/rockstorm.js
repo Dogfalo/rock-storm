49458cc6eeb754176bc8fc64a641e6160fab5cfb
Alvin Wang <alvin7@gmail.com>
2014-12-17 15:26:07 -0500

    working ground collision

--- static/js/rockstorm.js
+++ static/js/rockstorm.js
@@ -1,36 +1,56 @@
 console.log('You are playing Rock Storm the rockiest game on Earth');
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var player;
+var ground;
+var platforms;
 
 function preload() {
-  game.load.image('smiley', 'assets/images/smiley.png');
+   game.load.image('smiley', 'assets/images/smiley.png');
+   game.load.image('ground', 'assets/images/ground.png');
 }
 
 function create() {
-  player = game.add.sprite(0, 0, 'smiley');
-  //  We're going to be using physics, so enable the Arcade Physics system
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-  game.physics.arcade.enable(player);
+   //  We're going to be using physics, so enable the Arcade Physics system
+   game.physics.startSystem(Phaser.Physics.ARCADE);
+
+   player = game.add.sprite(0, 0, 'smiley');
+   platforms = game.add.group();
+   platforms.enableBody = true;
+   ground = platforms.create(0, game.world.height - 64, 'ground');
+
+
+   game.physics.arcade.enable(player);
+   ground.body.immovable = true;
+   ground.scale.setTo(.2, .2);
+
+   //  Player physics properties. Give the little guy a slight bounce.
+   player.body.bounce.y = 0.2;
+   player.body.gravity.y = 300;
+   player.body.collideWorldBounds = true;
 }
 
 function update() {
-  cursors = game.input.keyboard.createCursorKeys();
-  if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
- 
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
- 
-    }
-  
-    else 
-    {
-        player.body.velocity.x = 0;
- 
-    }
+   // Set collision with platform
+   game.physics.arcade.collide(player, platforms);
+
+   cursors = game.input.keyboard.createCursorKeys();
+   if (cursors.left.isDown)
+   {
+      //  Move to the left
+      player.body.velocity.x = -150;
+
+   }
+   else if (cursors.right.isDown)
+   {
+      //  Move to the right
+      player.body.velocity.x = 150;
+
+   }
+
+   else 
+   {
+      player.body.velocity.x = 0;
+
+   }
 }
