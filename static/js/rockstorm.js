1e833eea1cd8274ae946b8e0093509307aa5b009
jmiedel <jmiedel@andrew.cmu.edu>
2014-12-17 15:06:28 -0500

    added sprite

--- static/js/rockstorm.js
+++ static/js/rockstorm.js
@@ -3,9 +3,11 @@ console.log('You are playing Rock Storm the rockiest game on Earth');
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('smiley', 'assets/images/smiley.png');
 }
 
 function create() {
+  game.add.sprite(0, 0, 'smiley');
 }
 
 function update() {
